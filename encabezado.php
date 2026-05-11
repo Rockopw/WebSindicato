9d506927c514e0f7e8625dcc8e33df3ca2390fcc
Ruben Galeano <rubengaleano83@gmail.com>
2026-05-11 20:44:21 -0300

    cambio icono

--- encabezado.php
+++ encabezado.php
@@ -11,4 +11,4 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-</head>
+</head> 
